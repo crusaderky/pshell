724060724f177f98d4ee226c7a99b1bbbd159126
Jacob Lin <jacob.lin@ymail.com>
2019-01-27 20:39:49 +0000

    Add unitests for kill() around SIGTERM handling

---
+++ pshell/tests/data/sleep20_sigterm_delay5.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+"""Simple script that executes for 20s, and gracefully shuts down in 5s once
+SIGTERM is received.
+"""
+import os
+import signal
+import sys
+import time
+
+
+def _handler(signum, _frame):
+    """Print the incoming signal, sleep for 5s then gracefully exit."""
+    print(f'Receive signal {signum}')
+    time.sleep(5)
+    sys.exit(0)
+
+
+def main():
+    """Register signal handler then sleep 1s for 20 times."""
+    pid = os.getpid()
+    signal.signal(signal.SIGTERM, _handler)
+
+    for i in range(1, 20 + 1):
+        time.sleep(1)
+        print(f'{pid}: count {i}')
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
---
+++ pshell/tests/data/sleep20_sigterm_ignore.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Simple script that executes for 20s and ignores SIGTERM.
+"""
+import os
+import signal
+import sys
+import time
+
+
+def _handler(signum, _frame):
+    """Print the incoming signal, then do nothing."""
+    print(f'Receive signal {signum}')
+
+
+def main():
+    """Register signal handler then sleep 1s for 20 times."""
+    pid = os.getpid()
+    signal.signal(signal.SIGTERM, _handler)
+
+    for i in range(1, 20 + 1):
+        time.sleep(1)
+        print(f'{pid}: count {i}')
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
